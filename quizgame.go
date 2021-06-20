ccf8d1cdbbcf1de45996a79fd1a024979ea88b05
omerisildak <omerisildak0@gmail.com>
2021-06-20 17:09:17 +0300

    Comments

--- quizgame.go
+++ quizgame.go
@@ -15,34 +15,34 @@ type Problem struct {
 func readFromCsv(path string) []Problem {
 	f, err := os.Open(path)
 	if err != nil {
-		log.Fatal("Dosyayı Bulamadım"+path, err)
+		log.Fatal("Dosyayı Bulamadım"+path, err) // in case of error it would return error
 	}
-	defer f.Close()
+	defer f.Close() //its close when func has done  because of "defer"
 
-	csvReader := csv.NewReader(f)
+	csvReader := csv.NewReader(f) //reading
 	records, err := csvReader.ReadAll()
 	if err != nil {
 		log.Fatal("Parse edemedim"+path, err)
 	}
-	problems := []Problem{}
-	for _, line := range records {
+	problems := []Problem{}        //its our struct which holds all values
+	for _, line := range records { //we ititrate over records to add problems array which is type is Problem
 		problems = append(problems, Problem{line[0], line[1]})
 	}
 	return problems
 
 }
 func main() {
-	records1 := readFromCsv("problems.csv")
+	records1 := readFromCsv("problems.csv") //pathway its static but I will change with argv
 	score := 0
 	for i, problem := range records1 {
-		userAnswer := ""
+		userAnswer := "" //to initilaze we have used decoy variable
 		fmt.Printf("Problem #%d: %s = ", i+1, problem.question)
-		fmt.Scan(&userAnswer)
+		fmt.Scan(&userAnswer) //Takes input
 		if userAnswer == problem.answer {
 			score++
 		}
 	}
 
-	fmt.Printf("You scored %d out of %d.", score, len(records1))
+	fmt.Printf("You scored %d out of %d.", score, len(records1)) //Score
 
 }
